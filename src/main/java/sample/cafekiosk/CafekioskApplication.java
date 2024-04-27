08d9d1a6be7232e9b80a11c86cd04c318edd16d0
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-04-27 14:39:37 +0900

    initial commit

---
+++ src/main/java/sample/cafekiosk/CafekioskApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cafekiosk2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cafekiosk2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sample/cafekiosk2/Cafekiosk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cafekiosk2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
